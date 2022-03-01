819ff892865a105024b7241a1d96d00af31da69f
ikMendoza <ikmendoza.cloudpanda@gmail.com>
2022-03-01 12:59:33 +0800

    added scroll view for add item

--- components/EditScreenInfo.tsx
+++ components/EditScreenInfo.tsx
@@ -1,7 +1,7 @@
 import * as WebBrowser from 'expo-web-browser';
 import React, { useState } from 'react';
 import { StyleSheet, TouchableOpacity } from 'react-native';
-import { TextInput } from 'react-native-gesture-handler';
+import { ScrollView, TextInput } from 'react-native-gesture-handler';
 
 import Colors from '../constants/Colors';
 import { MonoText } from './StyledText';
@@ -12,34 +12,36 @@ export default function EditScreenInfo({ path }: { path: string }, { item }: { i
     const [itemPrice, setPrice] = useState();
     const [itemCount, setCount] = useState();
     return (
-        <View>
-            <View style={styles.container}>
-                <Text style={{padding: 10, fontSize: 18, flex: 0.5}}>Item Name:</Text>
-                <TextInput
-                style={styles.textInput}
-                onChangeText={newName => setText(newName)}
-                defaultValue={itemName}
-                />
+        <ScrollView>
+            <View>
+                <View style={styles.container}>
+                    <Text style={{padding: 10, fontSize: 18, flex: 0.5}}>Item Name:</Text>
+                    <TextInput
+                    style={styles.textInput}
+                    onChangeText={newName => setText(newName)}
+                    defaultValue={itemName}
+                    />
+                </View>
+                <View style={styles.container}>
+                    <Text style={{padding: 10, fontSize: 18, flex: 0.5}}>Item Price:</Text>
+                    <TextInput
+                    style={styles.textInput}
+                    onChangeText={newPrice => setPrice(newPrice)}
+                    defaultValue={itemPrice}
+                    keyboardType="number-pad"
+                    />
+                </View>
+                <View style={styles.container}>
+                    <Text style={{padding: 10, fontSize: 18, flex: 0.5}}>Stock:</Text>
+                    <TextInput
+                    style={styles.textInput}
+                    onChangeText={newCount => setCount(newCount)}
+                    defaultValue={itemCount}
+                    keyboardType="number-pad"
+                    />
+                </View>
             </View>
-            <View style={styles.container}>
-                <Text style={{padding: 10, fontSize: 18, flex: 0.5}}>Item Price:</Text>
-                <TextInput
-                style={styles.textInput}
-                onChangeText={newPrice => setPrice(newPrice)}
-                defaultValue={itemPrice}
-                keyboardType="number-pad"
-                />
-            </View>
-            <View style={styles.container}>
-                <Text style={{padding: 10, fontSize: 18, flex: 0.5}}>Stock:</Text>
-                <TextInput
-                style={styles.textInput}
-                onChangeText={newCount => setCount(newCount)}
-                defaultValue={itemCount}
-                keyboardType="number-pad"
-                />
-            </View>
-        </View>
+        </ScrollView>
     );
 }
 
